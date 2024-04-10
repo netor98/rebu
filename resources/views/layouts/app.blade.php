eb1d2900ccf770b90413b8cc6b99414430941f56
Nestor Padilla <pcu18021379@gmail.com>
2024-04-10 06:11:15 -0600

    as

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>REBU - @yield('titulo')</title>
-        @vite('resources/css/app.css')
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
+
+        @vite('resources/css/app.css')
 
     </head>
     <body>
